a56c3d7609063d9d2fe7ba2007cf871da08fdf8d
Gabriel <gabe.dev.web@gmail.com>
2018-12-08 12:59:16 +0100

    Updates

---
+++ precache-manifest.b7b204779c8a99f753669a812f3139e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76e806f8c1811b9ead3e",
+    "url": "/static/css/main.17d2657b.chunk.css"
+  },
+  {
+    "revision": "76e806f8c1811b9ead3e",
+    "url": "/static/js/main.76e806f8.chunk.js"
+  },
+  {
+    "revision": "d1ae976a3b95eb4a15f5",
+    "url": "/static/js/1.d1ae976a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ea20855f56f299db8653295a64a7fff",
+    "url": "/index.html"
+  }
+];
